87fcb66819665fcb530eed66806f49a0fd33b530
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-03-23 06:14:58 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OmK4yH8o0SKbfbdrGwGdw9bIN0feJaWfMLxXiemeYPsil9uZHGdBBKXXqHbEUupXNx2qsw6v3DSFHdhOYpwuO0F00VcK3d3Qf',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
