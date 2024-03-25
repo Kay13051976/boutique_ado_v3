a61c62143a99956932280099909e211231313b41
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-03-25 08:10:28 +0000

    deployment part 1

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,11 @@ SECRET_KEY = 'django-insecure-bw!zmjf0oy!70cc80c)u@l3d@imdjtjumvxc0u907v_=d%=(53
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-kay13051976-boutiqueado-i9lmhhlimim.ws-eu110.gitpod.io',]
+ALLOWED_HOSTS = [
+    '8000-kay13051976-boutiqueado-i9lmhhlimim.ws-eu110.gitpod.io',
+    'boutique-ado-v3-ww-2a4dbbfa1a55.herokuapp.com',
+    'localhost'
+]
 
 
 # Application definition
@@ -46,7 +51,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
-    
+
     # Other
     'crispy_forms'
 ]
@@ -71,7 +76,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
-            ],
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,13 +123,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
